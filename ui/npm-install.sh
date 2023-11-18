8c1b5abdb921deabc34a63e7140a256a8c8613fd
Eric Anderson <eric@aryn.ai>
2023-11-17 16:12:19 -0800

    Hack around the 'Worker was terminated' error that sometimes happens when viewing PDF.
    Happens about 50% of the time on my laptop ubuntu/firefox
    Happens about 20% of the time on my laptop ubuntu/chromium
    
    Tried ~50 reloads post fix and don't see it (do see the console log message a bunch)

--- ui/npm-install.sh
+++ ui/npm-install.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+apt -y install --no-install-recommends patch
+apt clean
+rm -rf /var/lib/apt/lists/*
+
 npm install
 err=$?
 
-echo "DEBUG ${err}"
 if [[ ${err} != 0 ]]; then
     ls /root/.npm/_logs
     cat /root/.npm/_logs/*
+    exit 1
 fi
 
 npm cache clean --force
-exit "${err}"
+
+# TODO: ERIC - figure out the right fix for this issue.  Fails in viewPdf at ~50% on firefox/ubuntu
+# ~10% on chromium/ubuntu
+patch -p0 <pdf.worker.js.patch || exit 1
+# it's wrong once we patch and very confusing in the browser debuggers
+rm node_modules/pdfjs-dist/build/pdf.worker.js.map
