5660ec1a9a33e5ee0d111091b90c18c5375be406
Alex Meyer <alex@aryn.ai>
2024-01-28 18:06:08 -0800

    SSL for proxy and UI, and run-script.

--- run-ui.sh
+++ run-ui.sh
@@ -10,11 +10,33 @@ if [[ -z "${OPENAI_API_KEY}" ]]; then
     exit 1
 fi
 
-cd /home/pn/py-proxy
-poetry run python py_proxy/proxy.py &
+# FIXME: If we unify Dockerfile and git dirs, this will become simpler.
+if [[ -f /.dockerenv ]]; then
+    PROXYDIR=/home/pn/py-proxy
+    UIDIR=/home/pn/js-ui
+else
+    cd $(dirname $0)
+    DIR=$(pwd)
+    PROXYDIR="${DIR}/openai-proxy"
+    UIDIR="${DIR}/ui"
+fi
+
+function cleanup {
+    kill "${PROXYPID}"
+}
+
+cd "${PROXYDIR}"
+: ${HOST:=localhost}
+openssl req -batch -x509 -newkey rsa:4096 -days 10000 \
+-subj "/C=US/ST=California/O=Aryn.ai/CN=${HOST}" \
+-extensions v3_req -addext "subjectAltName=DNS:${HOST}" \
+-noenc -keyout "${HOST}-key.pem" -out "${HOST}-cert.pem"
+poetry run python py_proxy/proxy.py "${HOST}" &
+PROXYPID=$!
+trap cleanup EXIT
 
 tries=0
-while ! curl localhost:3000/healthz >/dev/null 2>&1; do
+while ! curl -k "https://${HOST}:3000/healthz" >/dev/null 2>&1; do
     echo "Demo-UI proxy not running after $tries seconds"
     if [[ $tries -ge 30 ]]; then
         echo "Demo UI probably broken, report a bug on slack"
@@ -24,11 +46,11 @@ while ! curl localhost:3000/healthz >/dev/null 2>&1; do
 done
 
 # run node ui
-cd /home/pn/js-ui
+cd "${UIDIR}"
 # Running the UI this way means that the html is unminified and hence easy to read
 # Since the UI is open source, there's little downside of doing this and it helps with
 # debugging.
-PORT=3001 BROWSER=none npm start
+PORT=3001 HTTPS=true BROWSER=none npm start | cat
 
 # These are the steps that would build the UI and serve it minified.  We
 # should move the build back into the Dockerfile now that we no longer need
